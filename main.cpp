c627967d4cfb6cb429891acb610eb10ab6333840
Anukul <anukul_thienkasemsuk@hotmail.com>
2017-11-07 15:42:47 +0700

    add Cmake

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
+// A simple program that computes the square root of a number
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include "TravisOctodadConfig.h"
 
-using namespace std;
-
-int main()
+int main (int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
-    cout << "Hello Travis" << endl;
-    return 0;
+  if (argc < 2)
+    {
+    fprintf(stdout,"%s Version %d.%d\n",
+            argv[0],
+            Tutorial_VERSION_MAJOR,
+            Tutorial_VERSION_MINOR);
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    return 1;
+    }
+  double inputValue = atof(argv[1]);
+  double outputValue = sqrt(inputValue);
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
 }
